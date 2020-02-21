bd9b29dcf1f7c744ecb92aa1edcbe337ca381a97
Rael Corrales <raelcorrales@gmail.com>
2020-02-20 16:36:46 -0800

    TravisCI

--- mathematics/mathematics.py
+++ mathematics/mathematics.py
@@ -1,12 +1,3 @@
-# A prime number is any whole number with exactly two factors one and itself, ex: 2, 7, 31
-# Write a function that returns whether a non-zero positive integer (>0) is prime (true/false)
-# 100 =>   1, 2 , 4, 5, 10, 20, 25, 50, 100 # this is not a prime number (aka composite) 
-# 7 => 1, 7                                 # this is a prime number
-# 2 => 1, 2                                 # prime
-# 49 => 1, 7, 49                            # not prime (composite)
-# 10 => 1, 2, 5, 10                         # not prime (composite)
-# 1 => 1                                    # not prime (composite) “exactly two factors”
- 
 def prime(n):
 	if n == 1:
 		return False
@@ -24,6 +15,7 @@ def fib(n):
 		if len(a) >= 2:
 			a.pop(0)
 	return a[-1]
+
 def fib_array(n):
 	a = [0,1,1]
 	if n <= 3:
